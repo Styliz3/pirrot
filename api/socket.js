fce0573da308cf78c8175aa4a261d08bc95aa8e6
Styliz3 <karvisspelaa@gmail.com>
2025-09-07 03:53:31 +0300

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,3 +1,25 @@
+import { Server } from "socket.io";
+
+let io;
+
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, name: "Clipscribe AI", time: new Date().toISOString() });
+  if (!io) {
+    io = new Server(res.socket.server, {
+      path: "/api/socket",
+    });
+    res.socket.server.io = io;
+
+    io.on("connection", (socket) => {
+      console.log("A user connected");
+
+      socket.on("message", (msg) => {
+        socket.broadcast.emit("message", msg);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+      });
+    });
+  }
+  res.end();
 }
