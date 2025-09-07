05d26c1ce045a4dd132bfc7e8e588b2f649f9921
Styliz3 <karvisspelaa@gmail.com>
2025-09-07 12:58:42 +0300

    Update sdk.js

--- api/sdk.js
+++ api/sdk.js
@@ -1,15 +1,14 @@
 // api/sdk.js
 export default async function handler(req, res) {
-  const clientId = process.env.PAYPAL_CLIENT_ID; // set in Vercel env
-  const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${encodeURIComponent(clientId)}&components=buttons&intent=capture`;
-  const js = `
+  const clientId = process.env.PAYPAL_CLIENT_ID;   // set this in Vercel
+  const sdk = `https://www.paypal.com/sdk/js?client-id=${encodeURIComponent(clientId)}&components=buttons&intent=capture`;
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`
     (function(){
       var s=document.createElement('script');
-      s.src='${sdkUrl}';
-      s.onload=function(){ if(window.initPayPalButtons) window.initPayPalButtons('${clientId}'); };
+      s.src='${sdk}';
+      s.onload=function(){ if(window.initPayPalButtons) window.initPayPalButtons(); };
       document.currentScript.parentNode.insertBefore(s, document.currentScript);
     })();
-  `;
-  res.setHeader('Content-Type','application/javascript');
-  res.send(js);
+  `);
 }
